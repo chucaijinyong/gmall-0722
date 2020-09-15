3d1d5d30635ece99587bfc4ae0397187103d7621
chucaijinyong <1561763825@qq.com>
2020-09-16 07:53:36 +0800

    加入微信实现微信支付

---
+++ gmall-order/src/main/java/com/atguigu/gmall/order/wxpay/PayLogMapper.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.order.wxpay;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 支付日志表 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-03-13
+ */
+public interface PayLogMapper extends BaseMapper<PayLog> {
+
+}
